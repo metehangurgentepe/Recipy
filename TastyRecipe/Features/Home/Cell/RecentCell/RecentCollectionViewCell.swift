39fa9ce1f0de7cf9561ae862920098b1e627ce3b
Mete <metehangurgentepe@gmail.com>
2024-09-10 13:31:57 +0300

    HomeVC RecentCell loading is done.

---
+++ TastyRecipe/Features/Home/Cell/RecentCell/RecentCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecentCollectionViewCell.swift
+//  TastyRecipe
+//
+//  Created by Metehan Gürgentepe on 9.09.2024.
+//
+
+import UIKit
+
+class RecentCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TastyRecipe/Features/Home/Cell/RecentCell/RecentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecentTableViewCell.swift
+//  TastyRecipe
+//
+//  Created by Metehan Gürgentepe on 9.09.2024.
+//
+
+import UIKit
+
+class RecentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
